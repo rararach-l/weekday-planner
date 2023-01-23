b7fd2a22cf03c55952416d361f015c7c67c1e94c
rararach-l <117470446+rararach-l@users.noreply.github.com>
2023-01-23 13:03:47 +0000

    work on timeblocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,26 @@ $("#currentDay").text(currentDateTime);
 //      In the html add the data-hour which represent which hour each element is
 //      Create variable for moment().format(H) (0 - 23) e.g. thisHour
 
+var hour = parseInt($(this).attr("data-hour"));
+var thisHour = parseInt(moment().format("H"));
+var timeblocks = $(".timeblock");
+
+timeblocks.each(function() {
+    var hour = $(this).attr("data-hour");
+    if (hour < thisHour) {
+        $(this).css("background-color", "#d3d3d3");
+    } else if (hour == thisHour) {
+        $(this).css("background-color", "#ff6961");
+    } else {
+        $(this).css("background-color", "#77dd77");
+    }
+});
+
 // 3. Save input to local storage
 //      create variable (an array) called inputs that will store all of the input data
-//      Add event listener to all save button
+var inputs = [];
+
+//      Add event listener to all save buttons
 //          Add event.preventDefault inside the click event listener
 //          Push the input value to inputs array with the format of { time: xx, input: xxx }
 //              Get the input value
@@ -21,6 +38,14 @@ $("#currentDay").text(currentDateTime);
 //              Stringify the inputs array
 //          Show feedback message to the user (optional)
 //          The feedback need to be dissappeared automatically
+
+$(".saveBtn").on("click", function(event) {
+    event.preventDefault();
+    var input = $(this).siblings("textarea").val();
+    var hour = $(this).parent().attr("data-hour");
+    var inputData = { time: hour, input: input, completed: $(this).prev().is(':checked') };
+    var index = -1;
+
 
 // 4. Load input from local storage when page load/refresh if there's any data in local storage
 //      var localStorageInput = get data from local storage
